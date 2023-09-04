195759a8d10d471d7a9dfe146f5a6a46465f6ced
Mykyta Paiuka <nikvitka.it@gmail.com>
2023-09-04 09:49:28 +0300

    automatization ask + check user data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const numberOfFilms = prompt('How much films did you see?', '');
+'use strict';
+
+const numberOfFilms = +prompt('How much films did you see?', '');
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -7,15 +9,28 @@ const personalMovieDB = {
   privat: false,
 }
 
-askUser();
 
-
-
-function askUser() {
+for (let i = 0; i < 2; i++) {
   const a = prompt('One of the last movies you watched?', '');
   const b = prompt('How much would you rate it?', '');
 
-  personalMovieDB.movies[a] = b;
+  if (a === '' || b === '' || a === null || b === null || a.length > 50) {
+    i--;
+    console.log('error');
+  } else {
+    personalMovieDB.movies[a] = b;
+  }
+}
+
+
+if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
+  console.log('Few films watched');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('You are a classic spectator');
+} else if (personalMovieDB.count >= 30) {
+  console.log('You are a cinephile');
+} else {
+  console.log('error');
 }
 
 
